d1b85ae4122081c43e0edbfda071dea798cef9a3
Jeet <makanijeet51@gmail.com>
2026-04-12 23:18:11 +0530

    initial commit

---
+++ src/Game.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class Game{
+    private Scanner scanner = new Scanner(System.in);
+    private boolean isRunning;
+    private Map map;
+    private Player player;
+
+    public void start(){
+        map = new Map(10,10);
+        player = new Player();
+        player.setPosition(5,5);
+
+        isRunning = true;
+        gameloop();
+    }
+
+    public void gameloop(){
+        while(isRunning){
+            processInput();
+            update();
+            render();
+
+            try{
+                Thread.sleep(500);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    private void processInput(){
+        System.out.print("move (WASD) : ");
+        String input = scanner.nextLine().toLowerCase();
+
+        int newX = player.getX();
+        int newY = player.getY();
+
+        switch(input){
+            case "w" : newY--; break;
+            case "a" : newX--; break;
+            case "s" : newY++; break;
+            case "d" : newX++; break;
+        }
+        if(map.isWalkable(newX,newY)){
+            player.setPosition(newX, newY);
+        }
+    }
+    private void update(){
+
+    }
+    private void render(){
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+        // fallback
+            for (int i = 0; i < 30; i++) System.out.println();
+        }
+        char[][] grid = map.getGrid();
+        for(int y = 0; y < grid.length; y++){
+            for(int x = 0; x < grid[y].length; x++){
+                if(x == player.getX() && y == player.getY()){
+                    System.out.print("@");
+                }else{
+                    System.out.print(grid[y][x]);
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
---
+++ src/Map.java
@@ -0,0 +1,37 @@
+public class Map {
+    private int height;
+    private int width;
+    private char[][] grid;
+    
+    Map(int width, int height){
+        System.out.println("Map Created!");
+        this.width = width;
+        this.height = height;
+        grid = new char[height][width];
+        generateMap();
+    }
+
+    private void generateMap(){
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                if(y == 0 || y == height - 1 || x == 0 || x == width - 1){
+                    grid[y][x] = '#'; // walls
+                }else{
+                    grid[y][x] = '.'; //floor
+                }
+            }
+        }
+    }
+    private void getTile(int x, int y){
+
+    }
+    public char[][] getGrid(){
+        return grid;
+    }
+    public boolean isWalkable(int x, int y){
+        if(x < 0 || y < 0 || y >= height || x >= width){
+            return false;
+        }
+        return grid[y][x] == '.';
+    }
+}
